63736d4039e52d57d7d44daf94811ac09df7debd
Sarunas <sarunas@sarunast.com>
2016-03-01 23:46:13 +0100

    Initial live

--- tests.webpack.js
+++
@@ -1,9 +0,0 @@
-//const testsContext = require.context('./src/app', true, /spec\.js$/)
-//testsContext.keys().forEach(testsContext)
-//
-//const srcContext = require.context('./src/app', true, /!(spec\.js)$/)
-//srcContext.keys().forEach(srcContext)
-
-// Grab all javascript files
-const srcContext = require.context('./src/app', true, /\.js$/)
-srcContext.keys().forEach(srcContext)
